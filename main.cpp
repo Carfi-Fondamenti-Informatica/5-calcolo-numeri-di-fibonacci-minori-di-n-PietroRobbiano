23bdb1ca1bfd5e91579b7972648666e732b37072
PietroRobbiano <100785610+PietroRobbiano@users.noreply.github.com>
2022-04-06 16:10:25 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,23 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n = 0;
+    int a = 1;
+    int b = 1;
+    float somma = 0;
+    cin >> n;
+
+    if(n >= 2) {
+        cout << "1" << endl;
+        cout << "1" << endl;
+        for ( int i=0; n >= (somma+a); i++) {
+            somma = a + b;
+            cout << somma << endl;
+            a = b;
+            b = somma;
+        }
+    } else {
+            cout << "errore" << endl;
+        }
+    return 0;
 }
